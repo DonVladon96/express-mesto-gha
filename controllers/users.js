2e28a15082cad2ee22a03ece0004684ac65c10fa
DonVladon96 <Petrov.Vladik2010@yandex.com>
2023-05-25 13:24:24 +0300

    Code-Review 2

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,15 @@
+const mongoose = require('mongoose');
+const http2 = require('http2');
+const { Error } = require('mongoose');
+
+const {
+  HTTP_STATUS_CREATED,
+  HTTP_STATUS_BAD_REQUEST,
+  HTTP_STATUS_NOT_FOUND,
+  HTTP_STATUS_INTERNAL_SERVER_ERROR,
+  HTTP_STATUS_OK,
+} = http2.constants;
+
 const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
@@ -6,33 +18,34 @@ module.exports.getUsers = (req, res) => {
       res.send(users);
     })
     .catch(() => {
-      res.status(500).send({ message: 'Server Error' });
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Server Error' });
     });
 };
 
 // вариант из вебинара Сергея Буртылева
 module.exports.getUserById = (req, res) => {
-  const { _id } = req.user;
-
+  // const { _id } = req.user;
+  const _id = 123456789;
   User.findById(_id)
     .orFail()
     // eslint-disable-next-line consistent-return
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: 'User is not found' });
+        return res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'User is not found' });
       }
       res.send(user);
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(400).send({ message: 'User id is not valid.' });
+      console.log(err);
+      if (err instanceof Error.CastError) {
+        res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'User id is not valid.' });
         return;
       }
-      if (err.name === 'DocumentNotFoundError') {
-        res.status(404).send({ message: 'User id is not found.' });
+      if (err instanceof Error.DocumentNotFoundError) {
+        res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'User id is not found.' });
         return;
       }
-      res.status(500).send({ message: 'Server Error.' });
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Server Error.' });
     });
 };
 
@@ -43,13 +56,13 @@ module.exports.createUser = (req, res) => {
   console.log(req.praktikum);
   User.create({ name, about, avatar })
     .orFail()
-    .then((user) => res.status(201).send(user))
+    .then((user) => res.status(HTTP_STATUS_CREATED).send(user))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'User data is not a create.' });
+      if (err instanceof Error.ValidationError) {
+        res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'User data is not a create.' });
         return;
       }
-      res.status(500).send({ message: 'Server error' });
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Server error' });
     });
 };
 
@@ -64,19 +77,19 @@ module.exports.updateProfile = (req, res) => {
     },
   )
     .orFail()
-    .then((user) => res.status(200).send(user))
+    .then((user) => res.status(HTTP_STATUS_OK).send(user))
     .catch((err) => {
-      if (err.message === 'CastError') {
-        res.status(400).send({ message: 'User data is not valid' });
+      if (err instanceof Error.CastError) {
+        res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'User data is not valid' });
         return;
       }
 
-      if (err.name === 'DocumentNotFoundError') {
-        res.status(404).send({ message: 'User id is not a found' });
+      if (err instanceof Error.DocumentNotFoundError) {
+        res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'User id is not a found' });
         return;
       }
 
-      res.status(500).send({ message: 'Server error.' });
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Server error.' });
     });
 };
 
@@ -88,18 +101,18 @@ module.exports.updateAvatar = (req, res) => {
     { avatar },
   )
     .orFail()
-    .then((user) => res.status(200).send(user))
+    .then((user) => res.status(HTTP_STATUS_OK).send(user))
     .catch((err) => {
-      if (err.message === 'CastError') {
-        res.status(400).send({ message: 'User data is not a valid' });
+      if (err instanceof Error.CastError) {
+        res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'User data is not a valid' });
         return;
       }
 
-      if (err.name === 'DocumentNotFoundError') {
-        res.status(404).send({ message: 'User is not a found' });
+      if (err instanceof Error.DocumentNotFoundError) {
+        res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'User is not a found' });
         return;
       }
 
-      res.status(500).send({ message: 'Server error.' });
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Server error.' });
     });
 };
