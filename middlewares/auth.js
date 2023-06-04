f178399872a2b80f67a15f4ddccb30a113605300
DonVladon96 <Petrov.Vladik2010@yandex.com>
2023-06-04 15:37:50 +0300

    rew 3.0

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ module.exports.validateToken = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'Kiano-Ruiz');
+    payload = jwt.verify(token, 'secret-person-key');
     console.log(payload);
   } catch (err) {
     return next(new ErrorUnauthorized('Authorization required'));
